b74548d6c7b66743aa7908f85f7709f61ad23976
PabloCabello <pablo.cabello5@icloud.com>
2021-09-15 13:56:35 +0100

    Edits

--- app/routes.js
+++ app/routes.js
@@ -18,5 +18,37 @@ router.post('/new-software-answer', function (req, res) {
   }
 })
 
+// Add your routes here - above the module.exports line
+router.post('/major-upgrade-answer', function (req, res) {
+
+  // Make a variable and give it the value from 'new-to-software'
+  var isMajorUpgrade = req.session.data['major-upgrade']
+
+  // Check whether the variable matches a condition
+  if (isMajorUpgrade  == "Yes"){
+    // Send user to next page
+    res.redirect('sme-v2/company/companies-house-question')
+  } else {
+    // Send user to ineligible page
+    res.redirect('sme-v2/software/add-ons')
+  }
+})
+
+// Add your routes here - above the module.exports line
+router.post('/add-on-answer', function (req, res) {
+
+  // Make a variable and give it the value from 'new-to-software'
+  var isAddOn = req.session.data['add-on']
+
+  // Check whether the variable matches a condition
+  if (isAddOn  == "Yes"){
+    // Send user to next page
+    res.redirect('sme-v2/company/companies-house-question')
+  } else {
+    // Send user to ineligible page
+    res.redirect('sme-v2/software/ineligible')
+  }
+})
+
 
 module.exports = router
